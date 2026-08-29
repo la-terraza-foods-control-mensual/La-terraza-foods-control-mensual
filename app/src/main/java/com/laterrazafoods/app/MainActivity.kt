af5c8f7f9f7894755faf0bee737636767b3d3e95
la-terraza-foods-control-mensual <taxiplazapuentealto@gmail.com>
2026-08-29 10:19:48 -0400

    Fix formatting issue in MainActivity.kt

--- app/src/main/java/com/laterrazafoods/app/MainActivity.kt
+++ app/src/main/java/com/laterrazafoods/app/MainActivity.kt
@@ -17,7 +17,7 @@ data class Venta(
     val efectivoSinIva: Long,
     val tarjeta: Long,
     val transferenciaIva: Long,
-    val transferenciaSinIva: Long
+    val transferenciaSinIva: Long,
     val glosa: String
 )
 
